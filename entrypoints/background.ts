1c6539d79f17390138882511b1d4537dc3f8ba13
arshnoor1411 <arshnoor@infocusp.com>
2024-11-06 17:13:17 +0530

    fix: added message to console

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,27 +1,18 @@
 export default defineBackground(() => {
   console.log('Click Recorder extension installed');
 
-  chrome.permissions.request({
-    origins: [window.location.origin + "/"]
-  }, (granted) => {
-    if (granted) {
-      console.log("Permission granted for", window.location.origin);
-    } else {
-      console.log("Permission denied for", window.location.origin);
-    }
+  chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.set({ clicks: [] })
   })
 
   chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === "click") {
-      chrome.storage.local.get(["clicks"], (result) => {
-        console.log("Retrieved clicks:", result)
-        const clicks = result.clicks || [];
-        clicks.push(message.data);
-
-        chrome.storage.local.set({ clicks }, () => {
-          console.log("Clicks updated:", clicks);
-        });
-      });
+    console.log("Message---->", message)
+    if (message.type === 'CLICK_RECORDED') {
+      chrome.storage.local.get(['clicks'], (result) => {
+        const clicks = result.clicks || []
+        clicks.push(message.data)
+        chrome.storage.local.set({ clicks })
+      })
     }
-  });
+  })
 });
